84f750a7925ace70afda26cc32857342bd47488e
Andres Banda <abanda@cogcs.com>
2021-08-09 23:09:00 -0500

    Add social media links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,12 +11,18 @@ const Footer = () => {
       }}
     >
       <div style={{ margin: "20px auto 10px auto", color: "white" }}>
-        <FaFacebookF size="1.75rem" style={{ margin: "10px" }} />
-        <FaInstagram size="2rem" style={{ margin: "10px" }} />
-        <FaLinkedinIn size="2rem" style={{ margin: "10px" }} />
+        <a href="https://www.facebook.com/andro.b777/">
+          <FaFacebookF size="1.75rem" style={{ margin: "10px" }} />
+        </a>
+        <a href="https://www.instagram.com/andro_b7/">
+          <FaInstagram size="2rem" style={{ margin: "10px" }} />
+        </a>
+        <a href="https://www.linkedin.com/in/andres-banda">
+          <FaLinkedinIn size="2rem" style={{ margin: "10px" }} />
+        </a>
       </div>
       <p className="footer-text" style={{ margin: "15px auto" }}>
-        Developed by AB | All rights reserved
+        Developed by AB | All rights reserved ©
       </p>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import photo from "../profile_complete.jpeg";
+import logo from "./logo.png";
 
 const Home = () => {
   return (
@@ -27,10 +28,28 @@ const Home = () => {
       <div className="d-flex">
         <img src={photo} style={{ height: "100vh" }} />
       </div>
-      <div style={{ position: "absolute", top: "10px", right: "20px" }}>
-        <FaFacebookF size="1.75rem" style={{ margin: "10px" }} />
-        <FaInstagram size="2rem" style={{ margin: "10px" }} />
-        <FaLinkedinIn size="2rem" style={{ margin: "10px" }} />
+      <div
+        className="main-social"
+        style={{ position: "absolute", top: "10px", right: "20px" }}
+      >
+        <a href="https://www.facebook.com/andro.b777/">
+          <FaFacebookF size="1.75rem" style={{ margin: "10px" }} />
+        </a>
+        <a href="https://www.instagram.com/andro_b7/">
+          <FaInstagram size="2rem" style={{ margin: "10px" }} />
+        </a>
+        <a href="https://www.linkedin.com/in/andres-banda">
+          <FaLinkedinIn size="2rem" style={{ margin: "10px" }} />
+        </a>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "30px",
+          left: "30px",
+        }}
+      >
+        <img src={logo} width="80px" />
       </div>
     </div>
   );
